2560b6d6df704006060735abdac67b94ee551efd
Subhan Khan <subhank1997@gmail.com>
2022-05-23 14:24:54 -0400

    commit tree 0af37fb29dc6f2eff3d0ae68037c4d2db874b861

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(name, string) {
+  return name.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase()
+  );
+}
+
+function fuzzyMatch(name, testString) {
+  return name.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(name, soughtName) {
+  return name.filter((record) => record.name === soughtName);
+}
